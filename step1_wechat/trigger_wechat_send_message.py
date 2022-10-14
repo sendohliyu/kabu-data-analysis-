99d2b7e243196c2cd8c2dca5bb0f4f97513d09f3
sendohliyu <115762449+sendohliyu@users.noreply.github.com>
2022-10-13 20:42:06 -0700

    Update trigger_wechat_send_message.py
    login+find friend named liyu

--- step1_wechat/trigger_wechat_send_message.py
+++ step1_wechat/trigger_wechat_send_message.py
@@ -1,2 +1,10 @@
 print("Hello World")
 print("Hello Richard")
+# 导入模块
+from wxpy import *
+# 初始化机器人，扫码登陆
+bot = Bot()
+
+# 搜索名称含有 "游否" 的男性深圳好友
+my_friend = bot.friends().search('李煜', sex=MALE)[0]
+print(my_friend)
