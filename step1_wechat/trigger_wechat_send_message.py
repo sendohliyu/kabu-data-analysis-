21517371a0647c0cbc5b8e35a1d38fb53d3c0302
sendohliyu <115762449+sendohliyu@users.noreply.github.com>
2022-10-29 20:58:31 -0700

    Update trigger_wechat_send_message.py

--- step1_wechat/trigger_wechat_send_message.py
+++ step1_wechat/trigger_wechat_send_message.py
@@ -1,10 +1,35 @@
-print("Hello World")
-print("Hello Richard")
-# 导入模块
-from wxpy import *
-# 初始化机器人，扫码登陆
-bot = Bot()
+from pynput.keyboard import Key, Controller as key_cl  #键盘控制器
+from pynput.mouse import Button, Controller as mou_cl  #鼠标控制器
+import time
 
-# 搜索名称含有 "游否" 的男性深圳好友
-my_friend = bot.friends().search('李煜', sex=MALE)[0]
-print(my_friend)
+# 键盘的控制函数
+def keyboard_input(string):
+    # 获取键盘权限
+    keyboard = key_cl()
+    # 设置数据类型
+    keyboard.type(string)
+    
+# 鼠标的控制函数
+def mouse_click():
+    # 获取鼠标权限
+    mouse = mou_cl()
+    # 模拟左键按下
+    mouse.press(Button.left)
+    # 模拟左键弹起
+    mouse.release(Button.left)
+
+# 实现消息的发送函数
+def send_message(number,string):
+    print("程序在五秒钟之后开始执行")
+    time.sleep(5)
+    keyboard = key_cl()
+
+    for i in range(number):
+        keyboard_input(string)
+        mouse_click()
+        time.sleep(0.3)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+
+if __name__ == '__main__':
+    send_message(10,"祝您万圣节愉快！")
