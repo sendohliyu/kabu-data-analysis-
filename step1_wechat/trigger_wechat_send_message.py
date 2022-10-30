552a6f6192b31924c99190b6b4264fb2c6f5ad89
Jonas_QD <richmondjonas@gmail.com>
2022-10-29 21:07:11 -0700

    add find
    add find feature

--- step1_wechat/trigger_wechat_send_message.py
+++ step1_wechat/trigger_wechat_send_message.py
@@ -1,10 +1,97 @@
-print("Hello World")
-print("Hello Richard")
-# 导入模块
-from wxpy import *
-# 初始化机器人，扫码登陆
-bot = Bot()
 
-# 搜索名称含有 "游否" 的男性深圳好友
-my_friend = bot.friends().search('李煜', sex=MALE)[0]
-print(my_friend)
+import time
+import os
+import sys
+import pandas as pd
+from pynput.keyboard import Key, Controller
+
+
+def keyboard_input(string):
+    # 获取键盘权限
+    keyboard = key_cl()
+    # 设置数据类型
+    keyboard.type(string)
+
+
+def mouse_click():
+    # 获取鼠标权限
+    mouse = mou_cl()
+    # 模拟左键按下
+    mouse.press(Button.left)
+    # 模拟左键弹起
+    mouse.release(Button.left)
+
+
+def send_message(number, message: str):
+    keyboard = key_cl()
+
+    for i in range(number):
+        keyboard_input(message)
+        mouse_click()
+        time.sleep(0.3)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+
+
+def check_message(file_name: str):
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = file_path + '/' + file_name
+    print(file_path)
+    if os.path.exists(file_path):
+        df = pd.read_csv(file_path)
+        return df
+    else:
+        print("No 'message.csv' can be found, please create such csv (with header) that includes two column: person, message")
+        sys.exit()
+
+
+def find_person(keyboard):
+    keyboard.press(Key.cmd.value)
+    keyboard.press('f')
+    keyboard.release('f')
+    keyboard.release(Key.cmd.value)
+
+
+def enter_text(message, keyboard):
+    for ele in message:
+        keyboard.press(ele)
+        keyboard.release(ele)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+
+def send_message_one_row(row, keyboard):
+    find_person(keyboard)
+    enter_text(row['person'], keyboard)
+    # find_person(keyboard)
+    enter_text(row['message'], keyboard)
+    # print(row['person'], row['message'])
+
+
+def send_messages(df):
+    keyboard = Controller()
+    for index, row in df.iterrows():
+        send_message_one_row(row, keyboard)
+
+
+if __name__ == '__main__':
+
+    message_file_name = "message.csv"
+    print("Checking if message.csv exists")
+
+    df = check_message(message_file_name)
+    print("message.csv exists")
+
+    assert len(df.columns) == 2
+    assert df.columns[0] == 'person'
+    assert df.columns[1] == 'message'
+    print("程序在五秒钟之后开始执行")
+    time.sleep(2)
+    send_messages(df)
+
+    # print("程序在五秒钟之后开始执行")
+    # time.sleep(5)
+    # send_message(2, "Test")
+
+    # mouse = Controller()
+    # print('The current pointer position is {0}'.format(mouse.position))
